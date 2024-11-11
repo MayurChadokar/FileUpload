11f349bb2801f3e084b535c3326ccccb32270d86
MayurChadokar <mayurchadokar14@gmail.com>
2024-11-11 10:32:42 +0530

    Updated Code for Error Handeling

--- models/file.js
+++ models/file.js
@@ -1,22 +1,10 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const  fileSchema= new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
+const fileSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  tag: { type: String, required: true },
+  imageURL: { type: String, required: true },
+}, { timestamps: true });
 
-    },
-    imageURL:{
-     type:String
-    },
-    tag:{
-        type:String
-    },
-    email:{
-        type:String,
-        required:true,
-    }
-
-});
-
-module.exports=  mongoose.model("file",fileSchema);
+module.exports = mongoose.model("File", fileSchema);
